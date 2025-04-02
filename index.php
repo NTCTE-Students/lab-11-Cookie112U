2e4c77888fb378b7ee2376d836af3e2bd427e201
cookie <cookie@dfsf.ru>
2025-04-03 00:11:07 +0500

    Лаб 11

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+print('<h1>Лабораторная работа: Регулярные выражения</h1>');
+print('<ul>
+    <li><a href="task1.php">Задание 1</a></li>
+    <li><a href="task2.php">Задание 2</a></li>
+    <li><a href="task3.php">Задание 3</a></li>
+    <li>Дополнительные задания</li>
+    <li><a href="work_1.php">Задание 1</a></li>
+    <li><a href="work_2.php">Задание 2</a></li>
+    <li><a href="work_3.php">Задание 3</a></li>
+</ul>');
+?>
